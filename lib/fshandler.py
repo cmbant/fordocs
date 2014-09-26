9e11792dbfd92294a2b15f726ff02638f21c11ea
Antony Lewis <antony@cosmologist.info>
2014-09-26 16:37:10 +0100

    fix for backslashes in paths when generating on Windows

--- lib/fshandler.py
+++ lib/fshandler.py
@@ -29,8 +29,8 @@ class FileSystemHandler(object):
         self._makeModulesDirectory()
         
     def copyAssets(self):
-        source_assets_directory = pth.join(pth.dirname(__file__), "templates", "assets")
-        output_assets_directory = pth.join(self._destination, "assets")
+        source_assets_directory = self._join(pth.dirname(__file__), "templates/assets")
+        output_assets_directory = self._join(self._destination, "assets")
         self._mkDir(output_assets_directory)
         try:
             shutil.rmtree(output_assets_directory)
@@ -43,7 +43,7 @@ class FileSystemHandler(object):
         if perspective == self.FROM_INDEX_FOLDER:
             return "#"
         else:
-            return    pth.join("..", "index.html")
+            return    self._join("..", "index.html")
         
     def classIndex(self, perspective):
         if perspective == self.FROM_CLASS_FOLDER:
@@ -52,7 +52,7 @@ class FileSystemHandler(object):
             prefix = "classes"
         else:
             prefix = "../classes"
-        return    pth.join(prefix, "_index.html")  # not that a class named index overwrites the class index
+        return    self._join(prefix, "_index.html")  # not that a class named index overwrites the class index
     
     def assetsDirectory(self, perspective):
         if perspective == self.FROM_INDEX_FOLDER:
@@ -69,7 +69,7 @@ class FileSystemHandler(object):
             prefix = "files"
         else:
             prefix = "../files"
-        return    pth.join(prefix, file_doc_name)
+        return    self._join(prefix, file_doc_name)
     
     def programDocForPath(self, programPath, perspective):
         program_doc_name = self.htmlNameForPath(programPath)
@@ -80,7 +80,7 @@ class FileSystemHandler(object):
         else:
             prefix = "../programs"
             
-        return    pth.join(prefix, program_doc_name)
+        return    self._join(prefix, program_doc_name)
     
     def moduleDocForName(self, moduleName, perspective):
         module_doc = self.makeHtml(moduleName)
@@ -91,7 +91,7 @@ class FileSystemHandler(object):
         else:
             prefix = "../modules"
             
-        return    pth.join(prefix, module_doc)
+        return    self._join(prefix, module_doc)
     
     def classDocForName(self, className, perspective):
         class_doc = self.makeHtml(className)
@@ -102,7 +102,7 @@ class FileSystemHandler(object):
         else:
             prefix = "../classes"
             
-        return    pth.join(prefix, class_doc)
+        return    self._join(prefix, class_doc)
     
     def pureFileName(self, filePath):
         file_only = pth.split(filePath)[1]
@@ -114,45 +114,49 @@ class FileSystemHandler(object):
     
     def getSaveFileName(self, filePath):
         html_file = self.htmlNameForPath(filePath)
-        return pth.join(self.files_directory, html_file)
+        return self._join(self.files_directory, html_file)
     
     def getSaveProgramName(self, programPath):
         html_program = self.htmlNameForPath(programPath)
-        return pth.join(self.programs_directory, html_program)
+        return self._join(self.programs_directory, html_program)
     
     def getSaveClassName(self, className):
         html_class = self.makeHtml(className)
-        return pth.join(self.classes_directory, html_class)
+        return self._join(self.classes_directory, html_class)
     
     def getSaveModuleName(self, moduleName):
         html_module = self.makeHtml(moduleName)
-        return pth.join(self.modules_directory, html_module)
+        return self._join(self.modules_directory, html_module)
     
     def getSaveIndexName(self):
-        return pth.join(self._destination, "index.html")
+        return self._join(self._destination, "index.html")
     
     def getSaveClassIndexName(self):
-        return pth.join(self.classes_directory, "_index.html")
+        return self._join(self.classes_directory, "_index.html")
     
     def _makeFilesDirectory(self):
-        files_directory = pth.join(self._destination, "files")
+        files_directory = self._join(self._destination, "files")
         self.files_directory = files_directory
         self._mkDir(files_directory)
     
     def _makeProgramsDirectory(self):
-        programs_directory = pth.join(self._destination, "programs")
+        programs_directory = self._join(self._destination, "programs")
         self.programs_directory = programs_directory
         self._mkDir(programs_directory)
     
     def _makeModulesDirectory(self):
-        modules_directory = pth.join(self._destination, "modules")
+        modules_directory = self._join(self._destination, "modules")
         self.modules_directory = modules_directory
         self._mkDir(modules_directory)
     
     def _makeClassesDirectory(self):
-        classes_directory = pth.join(self._destination, "classes")
+        classes_directory = self._join(self._destination, "classes")
         self.classes_directory = classes_directory
         self._mkDir(classes_directory)
+    
+    def _join(self,p1,p2):
+        #Make sure we use / in web links even if generating on Windows
+        return pth.join(p1,p2).replace(os.sep,'/')
     
     def _mkDir(self, dirName):
         if not pth.exists(dirName):
